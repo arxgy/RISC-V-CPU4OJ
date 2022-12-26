4c2c0072222685b5180dae9650e8f70506fe3182
1816303037@qq.com <1816303037@qq.com>
2022-12-26 14:34:52 +0800

    upd

---
+++ sim/testbench.v
@@ -0,0 +1,29 @@
+`timescale 1ns/1ps
+module testbench;
+
+reg clk;
+reg rst;
+
+riscv_top #(.SIM(1)) top(
+    .EXCLK(clk),
+    .btnC(rst),
+    .Tx(),
+    .Rx(),
+    .led()
+);
+
+initial begin
+  clk=0;
+  rst=1;
+  repeat(50) #1 clk=!clk;
+  rst=0; 
+  forever #1 clk=!clk;
+
+  $finish;
+end
+
+initial begin
+     #300000000 $finish;
+end
+
+endmodule
